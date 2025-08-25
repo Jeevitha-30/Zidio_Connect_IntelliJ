581e78728afe385ef7ce883702ba69af81889b69
Jeevitha S <jeevithasivan30@gmail.com>
2025-08-25 15:28:33 +0530

    Added RecruiterResponse class to backend

---
+++ demo/src/main/java/dto/RecruiterResponse.java
@@ -0,0 +1,12 @@
+package dto;
+
+public class RecruiterResponse {
+    public Long userId;
+    public String companyName;
+    public String recruiterName;
+    public String recruiterEmail;
+    public String recruiterPosition;
+    public String designation;
+    public String companyWebsite;
+    public String companyLocation;
+}
